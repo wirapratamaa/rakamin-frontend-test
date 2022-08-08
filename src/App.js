dff375322fd3fff2cd7ca71422cf6f061eb5af6d
wirapratamaa <wirapratama758@gmail.com>
2022-08-08 13:24:19 +0800

    add context component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import GlobalProvider from "./context/GlobalState";
 import Home from "./page/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/v1" element={<Home />} />
+      <Route
+        path="/v1"
+        element={
+          <GlobalProvider>
+            <Home />
+          </GlobalProvider>
+        }
+      />
       <Route path="*" element={<Navigate to="/v1" replace />} />
     </Routes>
   );
